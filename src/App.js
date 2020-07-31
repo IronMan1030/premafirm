4877242c80964e6b4199ade98d5b81c9c44f12b1
yangting1030 <55540823+yangting1030@users.noreply.github.com>
2020-07-31 22:58:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import SearchProducts from "./pages/SearchProducts";
+import ImportList from "./pages/ImportList";
+import MyProducts from "./pages/MyProducts";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="admin-panel">
+      <div className="d-flex">
+        <BrowserRouter>
+          <div className="left-panel">
+            <Sidebar />
+          </div>
+          <div className="right-panel">
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/searchProduct" component={SearchProducts} />
+              <Route exact path="/importList" component={ImportList} />
+              <Route exact path="/myProducts" component={MyProducts} />
+              <Route exact path="/orders" component={Orders} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/pages/ImportList/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ImportList() {
+  return (
+    <div className="mr-4">
+      <p className="menu-title">Import List</p>
+    </div>
+  );
+}
+
+export default ImportList;
